749cccefec31c230122292703fea3a3968e310e1
asiddi40 <asiddi40@cougarnet.uh.edu>
2025-10-24 16:22:16 -0500

    Fix various issues in HW2.js validation functions

--- hw2/HW2.js
+++ hw2/HW2.js
@@ -1,4 +1,4 @@
- /* 
+/* 
  Name: Aleena Siddiqui 
  Date created: 9/19/2025
  Date last edited: 9/26/2025
@@ -6,72 +6,68 @@
  Description: Homework 2 JS 
  */ 
 
-//dynamic date js code// 
+// dynamic date js code
 const d = new Date(); 
 let text = d.toLocaleDateString(); 
 document.getElementById("Today").innerHTML = text; 
 
-//range slider js code// 
-let slider = document.getElementById("range") 
-let output = document.getElementById("range-slider") 
+// range slider js code
+let slider = document.getElementById("range");
+let output = document.getElementById("range-slider");
 output.innerHTML = slider.value; 
 
-slider.oninput = function () {output.innerHTML = this.value;};
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
 
 // dob validation js code
 function validatedob() {
-dob=document.getElementById("dob");
- let date = new Date(dob.value);
- let maxDate = new Date().setFullYear(new Date()-120);
+  let dob = document.getElementById("dob");
+  let date = new Date(dob.value);
+  let maxDate = new Date();
+  maxDate.setFullYear(maxDate.getFullYear() - 120);
 
- if (date > new Date()) {
-    document.getElementById("dob-error").innerHTML = 
-    "Date cannot be in the future."
-    dob.value="";
+  if (date > new Date()) {
+    document.getElementById("dob-error").innerHTML = "Date cannot be in the future.";
+    dob.value = "";
     return false;
- } else if(date < new Date(maxDate)) {
-    document.getElementId("dob-error").innerHTML = 
-    "Date cannot be more than 120 years ago."
-    dob.value="";
+  } else if (date < maxDate) {
+    document.getElementById("dob-error").innerHTML = "Date cannot be more than 120 years ago.";
+    dob.value = "";
     return false;
- } else {
-   document.getElementId("dob-error").innerHTML =
-   "";
-  return true;
- }
+  } else {
+    document.getElementById("dob-error").innerHTML = "";
+    return true;
+  }
 }
 
 // ssn validation js code
 function validatessn() {
-   const ssn = document.getElementId("ssn").value;
+  const ssn = document.getElementById("ssn").value;
 
- // regex for ssn pattern
-   const ssnR = /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/;
+  // regex for ssn pattern
+  const ssnR = /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/;
 
- if(!ssnR.test(ssn)) {
-   document.getElementId("ssn-error").innerHTML =
-   "Please enter a valid Social Security Number.";
-  return false;
- } else {
-   document.getElementId("ssn-error").innerHTML =
-    "";
-  return true;
- }
+  if (!ssnR.test(ssn)) {
+    document.getElementById("ssn-error").innerHTML = "Please enter a valid Social Security Number.";
+    return false;
+  } else {
+    document.getElementById("ssn-error").innerHTML = "";
+    return true;
+  }
 }
 
 // address validation js code
 function validateaddress1() {
-   var ad1 = document.getElementId("address1").value;
- console.log(ad1);
- console.log(ad1.length);
+  var ad1 = document.getElementById("address1").value;
+  console.log(ad1);
+  console.log(ad1.length);
 
- if (ad1.length < 2) {
-    document.getElementId("address1-error".innerHTML =
-    "Please enter Address Line.";
-  return false;
- } else {
-    document.getElementId("address1-error").innerHTML =
-    "";
-  return true;
- }
+  if (ad1.length < 2) {
+    document.getElementById("address1-error").innerHTML = "Please enter Address Line.";
+    return false;
+  } else {
+    document.getElementById("address1-error").innerHTML = "";
+    return true;
+  }
 }
