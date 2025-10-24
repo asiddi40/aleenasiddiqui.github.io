fbf5aeba4f52eab818b37c2d76292af1f9e5e8b6
asiddi40 <asiddi40@cougarnet.uh.edu>
2025-10-24 16:14:48 -0500

    Implement DOB and SSN validation functions
    Added date of birth and SSN validation functions to ensure user inputs are within valid ranges and formats.

--- hw2/HW2.js
+++ hw2/HW2.js
@@ -17,3 +17,46 @@ let output = document.getElementById("range-slider")
 output.innerHTML = slider.value; 
 
 slider.oninput = function () {output.innerHTML = this.value;};
+
+// dob validation js code
+function validatedob() {
+dob=document.getElementById("dob");
+ let date = new Date(dob.value);
+ let maxDate = new Date().setFullYear(new Date()-120);
+
+ if (date > new Date()) {
+    document.getElementById("dob-error").innerHTML = 
+    "Date cannot be in the future."
+    dob.value="";
+    return false;
+ } else if(date < new Date(maxDate)) {
+    document.getElementId("dob-error").innerHTML = 
+    "Date cannot be more than 120 years ago."
+    dob.value="";
+    return false;
+ } else {
+   document.getElementId("dob-error").innerHTML =
+   "";
+  return true
+ }
+}
+
+// ssn validation js code
+function validatessn() {
+   const ssn = document.getElementId("ssn").value;
+
+ // regex for ssn pattern
+   const ssnR = /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/;
+
+ if(!ssnR.test(ssn)) {
+   document.getElementId("ssn-error").innerHTML =
+   "Please enter a valid Social Security Number.";
+  return false;
+ } else {
+   document.getElementId("ssn-error").innerHTML =
+    "";
+  return true;
+ }
+}
+
+function validate
