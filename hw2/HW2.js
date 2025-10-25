c15d989e379a1e4f81124d28793c2b5aef9e3d45
asiddi40 <asiddi40@cougarnet.uh.edu>
2025-10-25 13:22:57 -0500

    Update HW2.js

--- hw2/HW2.js
+++ hw2/HW2.js
@@ -216,3 +216,32 @@ function confirmpwrd() {
 }
 
 // -- Review Button Code --
+function reviewInput() {
+    var formcontent = document.getElementById("signup");
+    var formoutput = "<table class='output'><th colspan = '3'> Review Your Information:</th>";
+    for (let i = 0; i < formcontent.length; i++) {
+        if (formcontent.elements[i].value !== "") {
+            switch (formcontent.elements[i].type) {
+                case "checkbox":
+                    if (formcontent.elements[i].checked) {
+                        formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>&#x2713;</td></tr>`;
+                    }
+                    break;
+                case "radio":
+                    if (formcontent.elements[i].checked) {
+                        formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>${formcontent.elements[i].value}</td></tr>`;
+                    }
+                    break;
+                default:
+                    formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>${formcontent.elements[i].value}</td></tr>`;
+            }
+        }
+    }
+    formoutput += "</table>";
+    document.getElementById("showInput").innerHTML = formoutput;
+}
+
+// -- Remove Review Data --
+function removeReview() {
+    document.getElementById("showInput").innerHTML = "";
+}
